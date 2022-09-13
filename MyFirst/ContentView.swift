041b0d5f4187d2e73ffef877bae9dfdf568ee76c
mataharimau <mataharimau@users.noreply.github.com>
2022-09-13 16:12:15 +1000

    Initial Commit

---
+++ MyFirst/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by Audrey Tam on 13/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Audrey Tam on 13/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
